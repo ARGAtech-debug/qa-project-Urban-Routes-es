5be5c981e6607cc82e4cab59a8ecd2796b106c51
Arturo <argacompanies@gmail.com>
2024-10-19 16:07:54 -0600

    Sprint 8

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-28e391e0-7992-4edd-b271-d9c31487b7ca.containerhub.tripleten-services.com/?lng=es'
+urban_routes_url = 'https://cnt-393ec1b7-33e6-4f68-a9dc-2a282e331b28.containerhub.tripleten-services.com/?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
